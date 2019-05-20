4371cd542be01eb16beb74c22016431789fbf476
Matthew <mf9646a@alumni.american.edu>
2019-05-20 17:50:12 -0400

    completed four out of seven objectives

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+  static sanitize(string) {
+    // string to array. remove caracter if not alphanumeric or dash single quote and space. join array to string.
+  }
+  static titleize() {
+
+  }
+
 }
