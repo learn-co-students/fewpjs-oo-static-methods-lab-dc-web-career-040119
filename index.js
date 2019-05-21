aada8e98e26d992b19c34b44aab292eb245029ba
Matthew <mf9646a@alumni.american.edu>
2019-05-21 12:05:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 class Formatter {
-  //add static methods here
+
   static capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
-}
-  static sanitize(string) {
-    // string to array. remove caracter if not alphanumeric or dash single quote and space. join array to string.
   }
-  static titleize() {
 
+  static sanitize(string) {
+    let sanitizedString = []
+    let arrayString = string.split('')
+    for (let i = 0; i < arrayString.length; i++)
+      if (arrayString[i].match(/^[a-zA-Z0-9 '-]*$/)) {
+        sanitizedString.push(arrayString[i])
+      }
+    return sanitizedString.join('')
+  }
+
+  static titleize(sentence) {
+    let exceptions = ['the','a','an','but','of','and','for','at','by','from']
+    let sentenceArray = sentence.split(" ")
+    let newSentence = []
+    for (let i = 0; i < sentenceArray.length; i++)
+      if (!exceptions.includes(sentenceArray[i])) {
+        newSentence.push(sentenceArray[i][0].toUpperCase() + sentenceArray[i].substr(1))
+      } else {
+        newSentence.push(sentenceArray[i])
+      }
+    let resultArray = newSentence.join(' ')
+    return resultArray[0].toUpperCase() + resultArray.substr(1)
   }
 
 }
